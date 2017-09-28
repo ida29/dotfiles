f587c784439b65389663c262936b0e2736b17854
ida_yuto <ida_yuto@artiza.co.jp>
2017-09-29 03:32:38 +0900

    Changed default shell for tmux to fish

--- fish/config.fish
+++ fish/config.fish
@@ -4,14 +4,7 @@ set -x GOPATH $HOME/go
 # git prompt
 set __fish_git_prompt_showdirtystate 'yes'
 set __fish_git_prompt_showstashstate 'yes'
-set __fish_git_prompt_showuntrackedfiles 'yes'
 set __fish_git_prompt_showupstream 'yes'
-set __fish_git_prompt_color_branch 6dc7ff
-
-# git status chars
-set __fish_git_prompt_char_dirtystate '!'
-set __fish_git_prompt_char_stagedstate 'a'
-set __fish_git_prompt_char_untrackedfiles '?'
-set __fish_git_prompt_char_stashstate 's'
-set __fish_git_prompt_char_upstream_ahead 'c'
-set __fish_git_prompt_char_upstream_behind '-'
+set __fish_git_prompt_showuntrackedfiles 'yes'
+set __fish_git_prompt_show_informative_status 'yes'
+set __fish_git_prompt_showcolorhints 'yes'
