79cf09070a1d6397537afdbfba9823ac30619e0e
ida_yuto <ida_yuto@artiza.co.jp>
2018-06-19 16:50:35 +0900

    Add .ctags

--- install.bat
+++ install.bat
@@ -1,3 +1,4 @@
 mklink %HOMEPATH%"\.vimrc" %HOMEPATH%"\dotfiles\.vimrc"
+mklink %HOMEPATH%"\.ctags" %HOMEPATH%"\dotfiles\.ctags"
 mklink /D %HOMEPATH%"\.vim\colors" %HOMEPATH%"\dotfiles\colors"
 exit 0
