83815c1e7da46a22ee65187a3b46d58457cc2ddc
EC2 Default User <ec2-user@ip-172-31-27-127.ap-southeast-1.compute.internal>
2019-02-17 10:41:53 +0000

    Add install script for AWS EC2 instance.

---
+++ install-ec2.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+ln -sf ~/dotfiles/.vimrc ~/.vimrc
+#ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -sf ~/dotfiles/.ctags ~/.ctags
+ln -sf ~/dotfiles/colors/ ~/.vim/
+#ln -sf ~/dotfiles/fish/config.fish ~/.config/fish/config.fish
+#ln -sf ~/dotfiles/fish/functions/fish_prompt.fish ~/.config/fish/functions/fish_prompt.fish
