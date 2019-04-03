21ce952e678801b5c65e29b018bf23f1484aeb05
ida <ida@localhost.localdomain>
2019-04-03 16:36:02 +0900

    Fix the part which does not work well with CentOS 6.2

--- install.sh
+++ install.sh
@@ -3,6 +3,8 @@
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/.ctags ~/.ctags
+mkdir -p ~/.vim
 ln -sf ~/dotfiles/colors/ ~/.vim/
 ln -sf ~/dotfiles/fish/config.fish ~/.config/fish/config.fish
+mkdir -p ~/.config/fish/functions
 ln -sf ~/dotfiles/fish/functions/fish_prompt.fish ~/.config/fish/functions/fish_prompt.fish
