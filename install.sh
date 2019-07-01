e2c7761b61011cb9f480ba11703c12afc6e53663
ida yuto <ida_yuto@artiza.co.jp>
2019-07-01 00:56:21 +0000

    [add]fisherのインストールを追加

--- install.sh
+++ install.sh
@@ -1,5 +1,4 @@
-#!/bin/sh
-
+#!/bin/bash
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/.ctags ~/.ctags
@@ -8,3 +7,4 @@ ln -sf ~/dotfiles/colors/ ~/.vim/
 ln -sf ~/dotfiles/fish/config.fish ~/.config/fish/config.fish
 mkdir -p ~/.config/fish/functions
 ln -sf ~/dotfiles/fish/functions/fish_prompt.fish ~/.config/fish/functions/fish_prompt.fish
+curl https://git.io/fisher --create-dirs -sLo ~/.config/fish/functions/fisher.fish
