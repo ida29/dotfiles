4e7a5f1926feeadd1d46dadfa290adf8737cc838
Yuto Ida <y.ida898@gmail.com>
2025-09-06 10:59:15 +0900

    update

--- fish/config.fish
+++ fish/config.fish
@@ -3,6 +3,8 @@ set -gx PATH "$VOLTA_HOME/bin" $PATH
 set -gx PATH $HOME/.claude/local $PATH
 fish_add_path /opt/homebrew/opt/postgresql@16/bin
 starship init fish | source
+set -gx BACKLOG_DOMAIN (cat ~/.backlog_domain)
+set -gx BACKLOG_API_KEY (cat ~/.backlog_key)
 set -gx GEMINI_API_KEY (cat ~/.gemini_key)
 function cc
   claude --dangerously-skip-permissions $argv
@@ -10,4 +12,3 @@ end
 
 # Neovide aliases
 alias nv='neovide'
-  
